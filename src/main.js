6830b85e248504df5fa4fd67f35c1d9e4663d4fa
Jakub Stříbrný <35335130+kubajznik@users.noreply.github.com>
2025-03-31 16:42:24 +0200

    use router

--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import './style.css'
 import App from './App.vue'
-import PrimeVue from 'primevue/config';
-import Aura from '@primevue/themes/aura';
-import {Button} from "primevue";
+import PrimeVue from 'primevue/config'
+import Aura from '@primevue/themes/aura'
+import {Button} from "primevue"
+import EventOverview from "./components/EventOverview.vue"
+import {createRouter, createWebHistory} from 'vue-router'
+
 
 const app = createApp(App)
 app.use(PrimeVue, {
@@ -11,5 +14,16 @@ app.use(PrimeVue, {
         preset: Aura
     }
 })
-app.component('Button', Button);
+
+const routes = [
+    {path: '/', component: EventOverview},
+]
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+})
+
+app.use(router)
+app.component('Button', Button)
 app.mount('#app')
